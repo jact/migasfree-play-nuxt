80187a93f7864d010c7a0f37c4132dea783e8c11
Jose Antonio Chavarría <jachavar@gmail.com>
2019-08-20 11:53:07 +0200

    Added initial info page

---
+++ pages/info/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="no-print">
+    <center>
+      <p>
+        <img src="/img/migasfree-play.svg" id="logo" />
+      </p>
+    </center>
+  </div>
+</template>
+
+<style scoped>
+#logo {
+  width: 20%;
+}
+</style>
