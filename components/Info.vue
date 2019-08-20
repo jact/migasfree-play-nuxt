1e7d63b9235f8340b4f3ed6204492487fc840fee
Jose Antonio Chavarría <jachavar@gmail.com>
2019-08-20 14:55:59 +0200

    Added Info component

---
+++ components/Info.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="no-print">
+    <center>
+      <p>
+        <img src="/img/migasfree-play.svg" id="logo" />
+      </p>
+
+      <p>{{ appName }} {{ appVersion }}</p>
+
+      <p>{{ appDescription }}</p>
+
+      <p>{{ appAuthors }}</p>
+    </center>
+  </div>
+</template>
+
+<style scoped>
+#logo {
+  width: 20%;
+}
+</style>
+
+<script>
+const app = require('../package.json')
+
+export default {
+  name: 'Info',
+  data() {
+    return {
+      appName: app.name,
+      appVersion: app.version,
+      appDescription: app.description,
+      appAuthors: app.author
+    }
+  }
+}
+</script>
