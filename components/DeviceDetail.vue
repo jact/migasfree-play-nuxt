f3b8568632857e6f473c5eaadc69a89dc5263dc7
Jose Antonio Chavarría <jachavar@gmail.com>
2019-08-20 11:54:27 +0200

    Improved component prototype

--- components/DeviceDetail.vue
+++ components/DeviceDetail.vue
@@ -11,9 +11,46 @@
       <div class="meta">{{ id }}</div>
       <div class="description">{{ description }}</div>
     </div>
-    <div class="extra content"></div>
+    <div class="extra content">
+      <p>
+        <span class="feature">BN</span>
+        <button
+          class="ui icon positive button"
+          data-tooltip="Instalar"
+          data-position="bottom center"
+        >
+          <i class="download icon"></i>
+        </button>
+      </p>
+      <p>
+        <span class="feature">Color</span>
+        <button
+          class="ui icon negative button"
+          data-tooltip="Desinstalar"
+          data-position="bottom center"
+        >
+          <i class="trash alternate icon"></i>
+        </button>
+      </p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.extra p {
+  padding: 4px 0;
+  line-height: 2.5em;
+}
+
+.feature {
+  font-size: 120%;
+  color: #000;
+}
+
+.extra p button {
+  float: right;
+}
+</style>
 
 <script>
 export default {
