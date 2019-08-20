f08739d6cc19809b6cd76568b2b23a029f7b262a
Jose Antonio Chavarría <jachavar@gmail.com>
2019-08-20 11:54:47 +0200

    Added Executions component

---
+++ components/Executions.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ExecutionDetail
+      v-for="(item, name, index) in this.$store.state.executions.log"
+      :key="index"
+      :id="name"
+      :command="item.command"
+      :text="item.text"
+    />
+  </div>
+</template>
+
+<script>
+import ExecutionDetail from '@/components/ExecutionDetail.vue'
+
+export default {
+  name: 'Executions',
+  components: {
+    ExecutionDetail
+  }
+}
+</script>
