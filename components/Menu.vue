e720d8f4522e1c0a2e246311f9b5a38c6c391685
Jose Antonio Chavarría <jachavar@gmail.com>
2019-08-22 11:43:32 +0200

    Added external link to migasfree-play documentation

--- components/Menu.vue
+++ components/Menu.vue
@@ -4,28 +4,13 @@
       <span class="ui big text">{{ computer }}</span>
     </a>
     <div class="right menu">
-      <router-link
-        to="/apps"
-        class="item"
-        data-tooltip="Apps"
-        data-position="bottom center"
-      >
+      <router-link to="/apps" class="item" data-tooltip="Apps" data-position="bottom center">
         <i class="large th icon"></i>
       </router-link>
-      <router-link
-        to="/devices"
-        class="item"
-        data-tooltip="Devices"
-        data-position="bottom center"
-      >
+      <router-link to="/devices" class="item" data-tooltip="Devices" data-position="bottom center">
         <i class="large print icon"></i>
       </router-link>
-      <router-link
-        to="/details"
-        class="item"
-        data-tooltip="Details"
-        data-position="bottom center"
-      >
+      <router-link to="/details" class="item" data-tooltip="Details" data-position="bottom center">
         <i class="large tasks icon"></i>
       </router-link>
       <router-link
@@ -36,15 +21,15 @@
       >
         <i class="large cog icon"></i>
       </router-link>
-      <router-link
-        to="/info"
-        class="item"
-        data-tooltip="Info"
-        data-position="bottom center"
-      >
+      <router-link to="/info" class="item" data-tooltip="Info" data-position="bottom center">
         <i class="large info circle icon"></i>
       </router-link>
-      <a class="item" data-tooltip="Help" data-position="bottom center">
+      <a
+        class="item"
+        data-tooltip="Help"
+        data-position="bottom center"
+        href="https://fun-with-migasfree.readthedocs.io/es/master/chapter10.html#migasfree-play"
+      >
         <i class="large question circle icon"></i>
       </a>
     </div>
