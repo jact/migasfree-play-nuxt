80bb2ad96ee9a25c9887c68354e729bb637ae79b
Jose Antonio Chavarría <jachavar@gmail.com>
2019-08-26 09:24:12 +0200

    Show links based on preferences

--- components/Menu.vue
+++ components/Menu.vue
@@ -4,14 +4,32 @@
       <span class="ui big text">{{ computer }}</span>
     </a>
     <div class="right menu">
-      <router-link to="/apps" class="item" data-tooltip="Apps" data-position="bottom center">
-        <i class="large th icon"></i>
+      <router-link
+        v-if="$store.state.preferences.showApps"
+        to="/apps"
+        class="item"
+        data-tooltip="Apps"
+        data-position="bottom center"
+      >
+        <i class="large th icon" />
       </router-link>
-      <router-link to="/devices" class="item" data-tooltip="Devices" data-position="bottom center">
-        <i class="large print icon"></i>
+      <router-link
+        v-if="$store.state.preferences.showDevices"
+        to="/devices"
+        class="item"
+        data-tooltip="Devices"
+        data-position="bottom center"
+      >
+        <i class="large print icon" />
       </router-link>
-      <router-link to="/details" class="item" data-tooltip="Details" data-position="bottom center">
-        <i class="large tasks icon"></i>
+      <router-link
+        v-if="$store.state.preferences.showDetails"
+        to="/details"
+        class="item"
+        data-tooltip="Details"
+        data-position="bottom center"
+      >
+        <i class="large tasks icon" />
       </router-link>
       <router-link
         to="/preferences"
@@ -19,18 +37,25 @@
         data-tooltip="Preferences"
         data-position="bottom center"
       >
-        <i class="large cog icon"></i>
+        <i class="large cog icon" />
       </router-link>
-      <router-link to="/info" class="item" data-tooltip="Info" data-position="bottom center">
-        <i class="large info circle icon"></i>
+      <router-link
+        v-if="$store.state.preferences.showInfo"
+        to="/info"
+        class="item"
+        data-tooltip="Info"
+        data-position="bottom center"
+      >
+        <i class="large info circle icon" />
       </router-link>
       <a
+        v-if="$store.state.preferences.showHelp"
         class="item"
         data-tooltip="Help"
         data-position="bottom center"
         href="https://fun-with-migasfree.readthedocs.io/es/master/chapter10.html#migasfree-play"
       >
-        <i class="large question circle icon"></i>
+        <i class="large question circle icon" />
       </a>
     </div>
   </div>
