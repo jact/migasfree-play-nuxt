030a25bc1eee5d369e6f6baccd757af1a3c21bb7
Jose Antonio Chavarría <jachavar@gmail.com>
2019-08-26 09:23:42 +0200

    Added methods

--- components/Preferences.vue
+++ components/Preferences.vue
@@ -3,15 +3,20 @@
     <div class="column">
       <form class="ui form">
         <div class="field">
-          <select>
-            <option v-for="item in languages" :key="item.code" :value="item.code">{{ item.name }}</option>
+          <select @change="setLanguage" v-model="language">
+            <option
+              v-for="item in languages"
+              :key="item.code"
+              :value="item.code"
+              :selected="item.code === $store.state.preferences.language"
+            >{{ item.name }}</option>
           </select>
         </div>
         <div class="inline field">
           <el-switch
-            v-model="showDetails"
+            v-model="showSyncDetails"
             active-text="Mostrar detalles al sincronizar"
-            @change="setShowDetails"
+            @change="setShowSyncDetails"
           />
         </div>
       </form>
@@ -24,16 +29,23 @@ export default {
   name: 'Preferences',
   data() {
     return {
+      language: this.$store.state.preferences.language,
       languages: [
         { code: 'es', name: 'Español' },
         { code: 'en', name: 'English' }
       ],
-      showDetails: false
+      showSyncDetails: this.$store.state.preferences.showSyncDetails
     }
   },
   methods: {
-    setShowDetails(value) {
-      this.showDetails = value
+    setLanguage() {
+      this.$store.commit('setLanguage', this.language)
+      this.$store.dispatch('savePreferences')
+    },
+    setShowSyncDetails(value) {
+      this.$store.commit('setShowSyncDetails', value)
+      this.showSyncDetails = this.$store.state.preferences.showSyncDetails
+      this.$store.dispatch('savePreferences')
     }
   }
 }
