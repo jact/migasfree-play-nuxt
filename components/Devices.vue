6950cc2528a8ac039994a4b12e01e0ae3076382c
Jose Antonio Chavarría <jachavar@gmail.com>
2019-08-29 14:13:44 +0200

    Added DeviceFilter component
    Loop DeviceDetail component

--- components/Devices.vue
+++ components/Devices.vue
@@ -1,22 +1,83 @@
 <template>
   <div class="ui stackable cards">
+    <DeviceFilter />
     <DeviceDetail
-      icon="/img/printer-net.png"
-      name="EPSON EPL-N2550"
-      id="15759"
-      connection="TCP"
-      description="Seminario 3P Redes y Sistemas CPD"
+      v-for="item in devicesByFilter"
+      :key="item.id"
+      :icon="icon(item.connection.name)"
+      :name="name(item)"
+      :id="item.name"
+      :connection="item.connection.name"
+      :description="description(item.data)"
+      :ip="ipAddress(item.data)"
+      :logical="item.logical"
     />
   </div>
 </template>
 
 <script>
+import DeviceFilter from '@/components/DeviceFilter.vue'
 import DeviceDetail from '@/components/DeviceDetail.vue'
 
 export default {
   name: 'Devices',
   components: {
+    DeviceFilter,
     DeviceDetail
+  },
+  data() {
+    return {
+      devices: []
+    }
+  },
+  computed: {
+    devicesByFilter() {
+      let results = this.devices
+
+      if (this.$store.state.filters.searchDevice)
+        results = results.filter(
+          device =>
+            device.model.name
+              .toLowerCase()
+              .includes(this.$store.state.filters.searchDevice) ||
+            JSON.parse(device.data)
+              .NAME.toLowerCase()
+              .includes(this.$store.state.filters.searchDevice)
+        )
+
+      if (this.$store.state.filters.onlyAssignedDevices)
+        results = results.filter(
+          device =>
+            this.$store.state.devices.assigned.filter(x => {
+              console.log(x, x.device.id, device.id)
+              x.device.id === device.id
+            }).length === 0
+        ) // FIXME
+
+      return results
+    }
+  },
+  methods: {
+    name(item) {
+      let data = JSON.parse(item.data)
+      if (data.NAME) return data.NAME
+      else return `${item.model.manufacturer.name} ${item.model.name}`
+    },
+    icon(connection) {
+      if (connection === 'TCP') return '/img/printer-net.png'
+      else return '/img/printer-local.png'
+    },
+    ipAddress(value) {
+      let data = JSON.parse(value)
+      return data.IP || ''
+    },
+    description(value) {
+      let data = JSON.parse(value)
+      return data.LOCATION || ''
+    }
+  },
+  mounted() {
+    this.devices = this.$store.state.devices.available
   }
 }
 </script>
