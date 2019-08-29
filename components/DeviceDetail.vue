89e96398c52caed349552ac23cacd3fbccef5bec
Jose Antonio Chavarría <jachavar@gmail.com>
2019-08-29 14:14:47 +0200

    Improved component

--- components/DeviceDetail.vue
+++ components/DeviceDetail.vue
@@ -1,37 +1,32 @@
 <template>
   <div class="fluid card">
     <div class="content">
-      <img
-        class="right floated tiny ui image"
-        :src="icon"
-        :data-tooltip="connection"
-        data-position="bottom center"
-      />
-      <div class="header">{{ name }}</div>
+      <img class="right floated tiny ui image" :src="icon" />
+      <div class="header" :data-tooltip="tooltip" data-position="bottom left">{{ name }}</div>
       <div class="meta">{{ id }}</div>
       <div class="description">{{ description }}</div>
     </div>
-    <div class="extra content">
-      <p>
-        <span class="feature">BN</span>
+    <div class="extra content" v-if="logical">
+      <p v-for="item in logical" :key="item.id">
+        <span class="feature">{{ featureName(item) }}</span>
         <button
           class="ui icon positive button right floated"
           data-tooltip="Instalar"
           data-position="bottom center"
         >
-          <i class="download icon"></i>
+          <i class="download icon" />
         </button>
       </p>
-      <p>
+      <!--p>
         <span class="feature">Color</span>
         <button
           class="ui icon negative button right floated"
           data-tooltip="Desinstalar"
           data-position="bottom center"
         >
-          <i class="trash alternate icon"></i>
+          <i class="trash alternate icon" />
         </button>
-      </p>
+      </p-->
     </div>
   </div>
 </template>
@@ -56,7 +51,21 @@ export default {
     name: { type: String, required: true },
     id: { type: String, required: true },
     connection: { type: String, required: true },
-    description: { type: String, required: false }
+    description: { type: String, required: false },
+    ip: { type: String, required: false },
+    logical: { type: Array, required: false }
+  },
+  computed: {
+    tooltip() {
+      if (this.ip) return `${this.connection} (${this.ip})`
+      else return this.connection
+    }
+  },
+  methods: {
+    featureName(item) {
+      if (item.alternative_feature_name) return item.alternative_feature_name
+      else return item.feature.name
+    }
   }
 }
 </script>
