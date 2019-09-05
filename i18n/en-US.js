a65e6885311e8f88579e393fbd7d1fe7655e62fa
Jose Antonio Chavarría <jachavar@gmail.com>
2019-09-05 14:35:28 +0200

    Added i18n messages

---
+++ i18n/en-US.js
@@ -0,0 +1,27 @@
+export default {
+  menu: {
+    apps: 'Apps',
+    devices: 'Devices',
+    details: 'Details',
+    preferences: 'Preferences',
+    info: 'Info',
+    help: 'Help'
+  },
+  login: {
+    title: 'User with privileges on the computer',
+    username: 'Username',
+    password: 'Password',
+    action: 'Login'
+  },
+  preferences: {
+    showSyncDetails: 'Show details when synchronizing'
+  },
+  info: {
+    lastSync: 'Last synchronization',
+    hardware: 'Hardware',
+    networkData: 'Network data',
+    migasfreeData: 'Migasfree data',
+    print: 'Print',
+    disks: 'disks'
+  }
+}
---
+++ i18n/es-ES.js
@@ -0,0 +1,27 @@
+export default {
+  menu: {
+    apps: 'Aplicaciones',
+    devices: 'Dispositivos',
+    details: 'Detalles',
+    preferences: 'Preferencias',
+    info: 'Información',
+    help: 'Ayuda'
+  },
+  login: {
+    title: 'Usuario con privilegios en el ordenador',
+    username: 'Nombre de usuario',
+    password: 'Contraseña',
+    action: 'Iniciar sesión'
+  },
+  preferences: {
+    showSyncDetails: 'Mostrar detalles al sincronizar'
+  },
+  info: {
+    lastSync: 'Última sincronización',
+    hardware: 'Hardware',
+    networkData: 'Datos de red',
+    migasfreeData: 'Datos de migasfree',
+    print: 'Imprimir',
+    disks: 'discos'
+  }
+}
