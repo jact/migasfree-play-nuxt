c62999dc42e5d50f73de846510ffb72001d2eb9e
jact <jachavar@gmail.com>
2020-08-18 14:13:44 +0200

    Added eslint:recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,17 @@ module.exports = {
     browser: true,
     node: true
   },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/essential',
+    // 'plugin:vue/strongly-recommended',
+    'eslint:recommended'
   ],
   // required to lint *.vue files
   plugins: ['vue']
